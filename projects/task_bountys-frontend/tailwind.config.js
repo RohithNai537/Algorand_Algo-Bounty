7a3838ea1024c21d7255bfaa0a3dc5c1c9194ecb
M.Rohith Naidu <155884778+RohithNai537@users.noreply.github.com>
2025-05-18 23:58:32 +0530

    Update tailwind.config.js
    Updated tailwind.config.js

--- projects/task_bountys-frontend/tailwind.config.js
+++ projects/task_bountys-frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // Enables dark mode using class strategy
+  darkMode: "class", // Enables dark mode using class strategy
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
